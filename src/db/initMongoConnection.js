2c61384fdf472b632bdccb816f9bc33867e01a4d
anna23q <anblinda4@gmail.com>
2025-06-12 13:33:05 +0300

    add index file

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,10 +2,8 @@ import mongoose from 'mongoose';
 
 export const initMongoConnection = async () => {
   try {
-    const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
-    const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
-
-    await mongoose.connect(connectionString);
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+    await mongoose.connect(uri);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection error:', error.message);
